afa7a268cc69bd8120944eae52e3352c65b467c2
Abu Bakr Samory <samory990@gmail.com>
2024-07-17 19:13:47 -0400

    Clear it all out

--- test.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-docker run -w /tmp -it alpine:edge sh -uelic 'addgroup -S lunaruser && adduser -S lunaruser -G lunaruser --shell /bin/sh && apk add yarn git python3 cargo neovim ripgrep alpine-sdk bash curl --update && LV_BRANCH='release-1.4/neovim-0.9' su -c "bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/release-1.4/neovim-0.9/utils/installer/install.sh) --no-install-dependencies" lunaruser && su -c /home/lunaruser/.local/bin/lvim lunaruser'
